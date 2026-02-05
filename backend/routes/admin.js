b708429cfb66f7598b571cf4a25a35231ba09303
om052 <prathmeshsawant288@gmail.com>
2026-02-05 12:10:50 +0530

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const adminAuth = require('../middleware/admin');
 const User = require('../models/User');
 const Script = require('../models/Script');
