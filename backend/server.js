a5851a144113cdd64c4820a5d00c8b326adf7ced
om052 <prathmeshsawant288@gmail.com>
2026-07-20 19:35:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -588,4 +588,7 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+server.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
